04ce9d2b75ac79358de0d3dd857b6a30df3cedd9
peterbergman <peter.bergman@live.se>
2014-06-11 09:52:30 +0200

    Refactor sensor js

--- espruino/sensor.js
+++ espruino/sensor.js
@@ -1,14 +1,14 @@
-var sensor;
-
-(function init() {
+function Sensor() {
   var ow = new OneWire(A1);
-  sensor = require("DS18B20").connect(ow);
-})();
-
-setInterval(function() {
-  var measurement = new Measurement(sensor.getTemp());
-  console.log(measurement);
-}, 1000);
+  this.sensorConnection = require("DS18B20").connect(ow);
+  this.interval = setInterval(function() {
+    var measurement = new Measurement(sensor.getTemp());
+    console.log(measurement);
+  }, 1000);
+  Sensor.prototype.stop = function() {
+    clearInterval(this.interval);
+  }
+}
 
 function Measurement(temperature) {
   this.date = null;
