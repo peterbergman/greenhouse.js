9ec30d64cfe5e90e94655375698ab4604df0b0f3
peterbergman <peter.bergman@live.se>
2014-07-01 14:24:07 +0200

    changed routing logic

--- web/app/routes/index.js
+++ web/app/routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.send('nothing yet...');
 });
+
+router.get('/measurements/startDate', function(req, res) {
+  res.send(400, {'error' : 'please supply a start date'});
+});
+
+router.get('/measurements/startDate/endDate', function(req, res) {
+  res.send(400, {'error' : 'please supply a start date and an end date'});
+});
+
+router.get('/measurements/startDate/:startDate/endDate', function(req, res) {
+  res.send(400, {'error' : 'please supply an end date'});
+});
+
+router.get('/measurements/startDate/:startDate', function(req, res) {
+  res.send(501, {'error' : 'not implemented yet'});
+});
+
+router.get('/measurements', function(req, res) {
+  res.send(501, {'error' : 'not implemented yet'});
+});
+
+router.get('/measurements/startDate/:startDate/endDate/:endDate', function(req, res) {
+  console.log(req.params);
+  var response = {};
+  var measurements = [];
+  var startDate = req.params.startDate;
+  var endDate = req.params.endDate;
+  response.startDate = startDate;
+  response.endDate = endDate;
+  measurements.push(new Measurement(22.3));
+  measurements.push(new Measurement(23.3));
+  measurements.push(new Measurement(24.3));
+  measurements.push(new Measurement(25.3));
+  response.measurements = measurements;
+  res.send(response);
+});
+
+function Measurement(temperature) {
+  this.date = null;
+  this.hour = null;
+  this.temperature = temperature;
+}
 
 module.exports = router;
