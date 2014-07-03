4653e4130f5d04a5c8c871acaef97bc43bff4739
peterbergman <peter.bergman@live.se>
2014-07-03 10:48:30 +0200

    tested to adjust ticks in chart

--- web/app/public/javascripts/app.js
+++ web/app/public/javascripts/app.js
@@ -1,60 +1,45 @@
-nv.addGraph(function () {
-    var chart = nv.models.lineChart().margin({
-        top: 30,
-        right: 20,
-        bottom: 50,
-        left: 20
-    }).showLegend(true).tooltipContent(function (key, y, e, graph) {
-        return '<h3>' + key + '</h3>' + '<p>' + e + '% at ' + y + '</p>'
-    });
-
-    chart.xAxis.tickFormat(function (d) {
-        return d3.time.format('%Y-%m-%d %H')(new Date(d))
-    });
-
-    d3.select('#chart1 svg')
-        .datum(data)
-        .transition().duration(500)
-        .call(chart);
-
-    nv.utils.windowResize(chart.update);
-    return chart;
-});
-
-function getData() {
+var currentDate = new Date(-1);
+(function getData() {
   $.ajax({
     url: 'http://ec2-54-88-73-46.compute-1.amazonaws.com:3000/measurements/startDate/2014-07-01',
     type: 'GET',
     dataType: 'jsonp',
-    jsonp: 'callback',
-    sucess: function(data) {
-      console.log(data);
-    },
-    callback: function(data) {
-      console.log(data);
-    }
+    jsonpCallback: 'jsonpCallback'
+  });
+})();
+
+function renderChart(data) {
+  nv.addGraph(function () {
+      var chart = nv.models.lineChart().margin({
+          top: 30,
+          right: 40,
+          bottom: 50,
+          left: 40
+      }).showLegend(true).tooltipContent(function (key, y, e, graph) {
+          return '<h3>' + key + '</h3>' + '<p>' + e + ' &deg;C at ' + y + '</p>'
+      });
+
+      chart.xAxis.ticks(5).tickFormat(function (d) {
+        return d3.time.format('%H')(new Date(d))
+      });
+      chart.yAxis.tickFormat(d3.format(',.1f'));
+      chart.xScale(d3.time.scale());
+
+      d3.select('#chart1 svg')
+          .datum(data)
+          .transition().duration(500)
+          .call(chart);
+
+      nv.utils.windowResize(chart.update);
+      return chart;
   });
 }
 
-data = [{
-    "values": [{
-        "x": 1025409600000,
-            "y": 2
-    }, {
-        "x": 1028088000000,
-            "y": 4
-    }, {
-        "x": 1030766400000,
-            "y": 1
-    }, {
-        "x": 1033358400000,
-            "y": 3
-    }, {
-        "x": 1036040400000,
-            "y": 0
-    }, {
-        "x": 1038632400000,
-            "y": 3
-    }],
-        "key": "Temperature",
-}]
+function jsonpCallback(response) {
+  data = [{'values': []}];
+  for (i in response) {
+    data[0].values.push({'x': new Date(response[i].date).getTime(), 'y': response[i].temperature});
+  }
+  data[0].key = 'Temperature';
+  renderChart(data);
+}
